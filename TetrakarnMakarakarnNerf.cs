42113b64d207609131005ec70c9cc9e8133cec34
Matthiew Purple <matthiewpurple@hotmail.com>
2023-05-22 15:18:04 +0200

    self only

--- TetrakarnMakarakarnNerf.cs
+++ TetrakarnMakarakarnNerf.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 using Il2Cpp;
 using tetrakarn_makarakarn_nerf;
 
-[assembly: MelonInfo(typeof(TetrakarnMakarakarnNerf), "Tetrakarn/Makarakarn nerf (1 ally)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonInfo(typeof(TetrakarnMakarakarnNerf), "Tetrakarn/Makarakarn nerf (self only)", "1.0.0", "Matthiew Purple")]
 [assembly: MelonGame("アトラス", "smt3hd")]
 
 namespace tetrakarn_makarakarn_nerf;
@@ -17,10 +17,10 @@ public class TetrakarnMakarakarnNerf : MelonMod
         {
             switch (id) {
                 case 69:
-                    __result = "Repels Magical attacks \nfor one ally once \nnext turn."; // Makarakarn
+                    __result = "Repels Magical attacks \nonce next turn."; // Makarakarn
                     break;
                 case 70:
-                    __result = "Repels Physiccal attacks \nfor one ally once \nnext turn."; // Tetrakarn
+                    __result = "Repels Physiccal attacks \nonce next turn."; // Tetrakarn
                     break;
             }
         }
@@ -35,10 +35,10 @@ public class TetrakarnMakarakarnNerf : MelonMod
             switch (id)
             {
                 case 34:
-                    __result = "Repels Magical attacks \nfor one ally\n once next turn."; // Magic Mirror
+                    __result = "Repels Magical attacks \nonce next turn."; // Magic Mirror
                     break;
                 case 35:
-                    __result = "Repels Physiccal attacks \nfor one ally\n once next turn."; // Attack Mirror
+                    __result = "Repels Physiccal attacks \nonce next turn."; // Attack Mirror
                     break;
             }
         }
@@ -49,5 +49,8 @@ public class TetrakarnMakarakarnNerf : MelonMod
     {
         datNormalSkill.tbl[69].targettype = 0; // Makes Makarakarn 1 target
         datNormalSkill.tbl[70].targettype = 0; // Makes Tetrakarn 1 target
+
+        datNormalSkill.tbl[69].targetrule = 1; // Makes Makarakarn only work on self
+        datNormalSkill.tbl[70].targetrule = 1; // Makes Tetrakarn only work on self
     }
 }
